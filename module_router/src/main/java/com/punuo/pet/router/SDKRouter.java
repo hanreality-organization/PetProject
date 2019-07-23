3190e816b357ae64398cbb6f9389eb3a74fba84b
han.chen <han.chen@husor.com>
2019-07-23 15:43:16 +0800

    WebViewActivity 修改

---
+++ module_router/src/main/java/com/punuo/pet/router/SDKRouter.java
@@ -0,0 +1,11 @@
+package com.punuo.pet.router;
+
+/**
+ * Created by han.chen.
+ * Date on 2019-07-23.
+ **/
+public class SDKRouter {
+    private static final String PREFIX = "/sdk";
+    public static final String ROUTER_WEB_VIEW_ACTIVITY = PREFIX + "/WebViewActivity";
+
+}
