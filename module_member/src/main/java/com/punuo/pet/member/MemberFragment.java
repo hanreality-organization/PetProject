0cb1976ac992fa012ba113712c54a0d1e11f3459
Kuiya <1144573342@qq.com>
2019-07-30 10:49:15 +0800

    我的界面布局

--- module_member/src/main/java/com/punuo/pet/member/MemberFragment.java
+++ module_member/src/main/java/com/punuo/pet/member/MemberFragment.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.alibaba.android.arouter.launcher.ARouter;
+import com.bumptech.glide.Glide;
 import com.punuo.pet.member.request.LogoutRequest;
 import com.punuo.pet.router.MemberRouter;
 import com.punuo.sys.sdk.account.AccountManager;
@@ -26,34 +27,41 @@ import com.punuo.sys.sdk.util.StatusBarUtil;
  * 我的页面
  **/
 @Route(path = MemberRouter.ROUTER_MEMBER_FRAGMENT)
-public class MemberFragment extends BaseFragment {
+public class MemberFragment extends BaseFragment implements View.OnClickListener{
 
-    private TextView mTitle;
     private ImageView mBack;
     private TextView mExitButton;
     private TextView mAddPetButton;
+    private TextView mNickEdit;
+    private ImageView mAvater;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mFragmentView = inflater.inflate(R.layout.fragment_member, container, false);
         initView();
+
         View statusBar = mFragmentView.findViewById(R.id.status_bar);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             statusBar.getLayoutParams().height = StatusBarUtil.getStatusBarHeight(getActivity());
             statusBar.setVisibility(View.VISIBLE);
             statusBar.requestLayout();
         }
+
         return mFragmentView;
     }
 
     private void initView() {
-        mBack = mFragmentView.findViewById(R.id.back);
-        mTitle = mFragmentView.findViewById(R.id.title);
+
         mExitButton = mFragmentView.findViewById(R.id.exit_button);
         mAddPetButton = mFragmentView.findViewById(R.id.add_pet_button);
-        mBack.setVisibility(View.GONE);
-        mTitle.setText("我的");
+        mNickEdit = mFragmentView.findViewById(R.id.user_nickname);
+        mAvater = mFragmentView.findViewById(R.id.user_avater);
 
+        mExitButton.setOnClickListener(this);
+        mAddPetButton.setOnClickListener(this);
+
+        /**
         mExitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -67,7 +75,17 @@ public class MemberFragment extends BaseFragment {
                         .navigation();
             }
         });
+         */
+
+
+        //设置用户头像
+        String avater = AccountManager.getUserInfo().avatar;
+        Glide.with(getActivity()).load(avater).into(mAvater);
+
+        //设置用户ID
     }
+
+
 
 
     private LogoutRequest mLogoutRequest;
@@ -107,5 +125,20 @@ public class MemberFragment extends BaseFragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if(id == R.id.exit_button){
+            logout(AccountManager.getSession());
+        }
+        else if(id == R.id.add_pet_button){
+            ARouter.getInstance().build(MemberRouter.ROUTER_ADD_PET_ACTIVITY).navigation();
+        }
+        else if(id == R.id.account){
+            ARouter.getInstance().build(MemberRouter.ROUTER_ACCOUNT_MANAGEMENT_ACTIVITY).navigation();
+        }
+
     }
 }
--- module_router/src/main/java/com/punuo/pet/router/MemberRouter.java
+++ module_router/src/main/java/com/punuo/pet/router/MemberRouter.java
@@ -13,5 +13,9 @@ public class MemberRouter {
     public static final String ROUTER_BIND_PHONE_ACTIVITY = PREFIX + "/BindPhoneActivity";
     public static final String ROUTER_ADD_PET_ACTIVITY = PREFIX + "/AddPetActivity";
 
+    public static final String ROUTER_ACCOUNT_MANAGEMENT_ACTIVITY = PREFIX + "AccountManagementActivity";
+    public static final String ROUTER_CHANGE_AVATER_ACTIVITY = PREFIX + "ChangeAvaterActivity";
+    public static final String ROUTER_SET_NICKNAME_ACTIVITY = PREFIX + "SetNickname";
+    public static final String ROUTER_SET_PASSWORD_ACTIVITY = PREFIX +"SetPassword";
 
 }
