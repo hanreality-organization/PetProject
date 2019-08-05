d3b8eb28ff6aa578c9e6465df77bcdbdf2533f75
han.chen <han.chen@husor.com>
2019-08-05 19:41:54 +0800

    引入 PagerSlidingTabStrip

--- module_circle/src/main/java/com/punuo/pet/cirlce/fragment/CircleFragment.java
+++ module_circle/src/main/java/com/punuo/pet/cirlce/fragment/CircleFragment.java
@@ -25,7 +25,7 @@ import java.util.ArrayList;
  */
 
 @Route(path = CircleRouter.ROUTER_CIRCLE_FRAGMENT)
-public class CircleFragment extends BaseFragment implements View.OnClickListener,ViewPager.OnPageChangeListener{
+public class CircleFragment extends BaseFragment implements View.OnClickListener, ViewPager.OnPageChangeListener {
 
     Button button1;
     Button button2;
@@ -42,29 +42,29 @@ public class CircleFragment extends BaseFragment implements View.OnClickListener
     Button[] btnArgs;
     //fragment的集合，对应的每个子界面
     ArrayList<Fragment> fragments;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        mFragmentView = inflater.inflate(R.layout.fragment_main_circle,container,false);
+        mFragmentView = inflater.inflate(R.layout.fragment_main_circle, container, false);
 
         initView();
         return mFragmentView;
 
 
-
     }
 
     //初始化布局
-    public void initView(){
+    public void initView() {
         myViewpager = mFragmentView.findViewById(R.id.circle_viewpager);
         button1 = mFragmentView.findViewById(R.id.button_contacts);
-        button2= mFragmentView.findViewById(R.id.button_mycircle);
+        button2 = mFragmentView.findViewById(R.id.button_mycircle);
         button3 = mFragmentView.findViewById(R.id.button_nearby);
         button4 = mFragmentView.findViewById(R.id.button_world);
-        button5 =mFragmentView.findViewById(R.id.button_add);
+        button5 = mFragmentView.findViewById(R.id.button_add);
 
         //初始化按钮数组
-        btnArgs = new Button[]{button1,button2,button3,button4,button5};
+        btnArgs = new Button[]{button1, button2, button3, button4, button5};
         //指示标签设置成蓝色
         cursor = mFragmentView.findViewById(R.id.image1);
         cursor.setBackgroundColor(Color.BLUE);
@@ -83,7 +83,7 @@ public class CircleFragment extends BaseFragment implements View.OnClickListener
         fragments.add(new WorldFragment());
         fragments.add(new AddFragment());
 
-        CircleFragmentPagerAdapter circleFragmentPagerAdapter = new CircleFragmentPagerAdapter(getFragmentManager(),fragments);
+        CircleFragmentPagerAdapter circleFragmentPagerAdapter = new CircleFragmentPagerAdapter(getFragmentManager(), fragments);
 
         myViewpager.setAdapter(circleFragmentPagerAdapter);
 
@@ -93,7 +93,7 @@ public class CircleFragment extends BaseFragment implements View.OnClickListener
         button2.setTextColor(Color.BLUE);
     }
 
-    public void resetButtonColor(){
+    public void resetButtonColor() {
 //        button2.setBackgroundColor(Color.parseColor("#DCDCDC"));
 //        button3.setBackgroundColor(Color.parseColor("#DCDCDC"));
 //        button4.setBackgroundColor(Color.parseColor("#DCDCDC"));
@@ -102,12 +102,13 @@ public class CircleFragment extends BaseFragment implements View.OnClickListener
         button3.setTextColor(Color.BLACK);
         button4.setTextColor(Color.BLACK);
     }
+
     @Override
     public void onPageSelected(int position) {
 
         //widthArgs的实例化
-        if(widthArgs==null){
-            widthArgs = new int[]{button1.getWidth(),button2.getWidth(),button3.getWidth(),button4.getWidth(),button5.getWidth()};
+        if (widthArgs == null) {
+            widthArgs = new int[]{button1.getWidth(), button2.getWidth(), button3.getWidth(), button4.getWidth(), button5.getWidth()};
         }
 
         //每次滑动首先重置按钮的颜色
@@ -120,39 +121,39 @@ public class CircleFragment extends BaseFragment implements View.OnClickListener
     @Override
     public void onClick(View view) {
         int id = view.getId();
-        if(id==R.id.button_contacts){
+        if (id == R.id.button_contacts) {
             myViewpager.setCurrentItem(0);
             cursorAnim(0);
-        } else if(id==R.id.button_mycircle){
+        } else if (id == R.id.button_mycircle) {
             myViewpager.setCurrentItem(1);
             cursorAnim(1);
-        }else if(id==R.id.button_nearby){
+        } else if (id == R.id.button_nearby) {
             myViewpager.setCurrentItem(2);
             cursorAnim(2);
-        }else if(id==R.id.button_world){
+        } else if (id == R.id.button_world) {
             myViewpager.setCurrentItem(3);
             cursorAnim(3);
-        }else if(id==R.id.button_add){
+        } else if (id == R.id.button_add) {
             myViewpager.setCurrentItem(4);
             cursorAnim(4);
         }
     }
 
     //指示器的跳转，传入当前所处当前所处的页面的下标
-    public void cursorAnim(int curItem){
+    public void cursorAnim(int curItem) {
         //每次调用，都将指示器的横坐标设置为0，即开始的位置
         cursorX = 0;
         //根据当前的curItem来设置指示器的宽度
         LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) cursor.getLayoutParams();
         //减去边距*2，对齐标题栏文字
-        lp.width = widthArgs[curItem]-btnArgs[0].getPaddingLeft()*2;
+        lp.width = widthArgs[curItem] - btnArgs[0].getPaddingLeft() * 2;
         cursor.setLayoutParams(lp);
         //循环获取当前页之前的所有页面的宽度
-        for(int i=0;i<curItem;i++){
+        for (int i = 0; i < curItem; i++) {
             cursorX = cursorX + btnArgs[i].getWidth();
         }
         //再加上当前页面的左边距，即为指示器当前应处的位置
-        cursor.setX(cursorX+btnArgs[curItem].getPaddingLeft());
+        cursor.setX(cursorX + btnArgs[curItem].getPaddingLeft());
     }
 
     @Override
