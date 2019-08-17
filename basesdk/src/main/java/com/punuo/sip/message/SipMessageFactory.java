9cf14085578bffb89b2c0e13240a12be513188d5
han.chen <han.chen@husor.com>
2019-08-17 15:06:42 +0800

    引入 rtp包

--- basesdk/src/main/java/com/punuo/sip/message/SipMessageFactory.java
+++ basesdk/src/main/java/com/punuo/sip/message/SipMessageFactory.java
@@ -9,7 +9,6 @@ import org.zoolu.sip.address.SipURL;
 import org.zoolu.sip.message.Message;
 import org.zoolu.sip.message.MessageFactory;
 import org.zoolu.sip.message.SipMethods;
-import org.zoolu.sip.provider.SipProvider;
 
 /**
  * Author chzjy
@@ -45,54 +44,40 @@ public class SipMessageFactory extends MessageFactory {
         return msg;
     }
 
-    private static Message createSubscribeRequest(SipProvider sip_provider,  //设备列表
-                                                  NameAddress to, NameAddress from) {
-        String via_addr = SipUserManager.getInstance().getViaAddress();
-        int host_port = SipUserManager.getInstance().getPort();
-        SipURL sipURL = new SipURL(via_addr, host_port);
-        NameAddress contact = new NameAddress(sipURL);
-        return createRequest(sip_provider, SipMethods.SUBSCRIBE, sipURL, to, from, contact, null);
+    public static Message createSubscribeRequest(NameAddress to, NameAddress from, String body) {
+        Message message = createRequest(SipUserManager.getInstance(), SipMethods.SUBSCRIBE, sipURL,
+                to, from, contact, null);
+        if (!TextUtils.isEmpty(body)) {
+            message.setBody(CONTENT_TYPE, body);
+        }
+        return message;
     }
 
-    public static Message createSubscribeRequest(SipProvider sip_provider,  //设备列表
-                                                 NameAddress to, NameAddress from, String body) {
-        Message msg = createSubscribeRequest(sip_provider, to, from);
-        msg.setBody(CONTENT_TYPE, body);
+    public static Message createOptionsRequest(NameAddress to, NameAddress from, String body) {
+        Message msg = createRequest(SipUserManager.getInstance(), SipMethods.OPTIONS, sipURL,
+                to, from, contact, null);
+        if (!TextUtils.isEmpty(body)) {
+            msg.setBody(CONTENT_TYPE, body);
+        }
         return msg;
     }
 
-    public static Message createOptionsRequest(SipProvider sip_provider,  //视频信息查询
-                                               NameAddress to, NameAddress from, String body) {
-        String via_addr = sip_provider.getViaAddress();
-        int host_port = sip_provider.getPort();
-        SipURL sipURL = new SipURL(via_addr, host_port);
-        NameAddress contact = new NameAddress(sipURL);
-        Message msg = createRequest(sip_provider, SipMethods.OPTIONS, sipURL, to, from, contact, null);
-        msg.setBody(CONTENT_TYPE, body);
+    public static Message createInviteRequest(NameAddress to, NameAddress from, String body) {
+        Message msg = createInviteRequest(SipUserManager.getInstance(), sipURL, to, from, contact, null);
+        if (!TextUtils.isEmpty(body)) {
+            msg.setBody(CONTENT_TYPE, body);
+        }
         return msg;
     }
 
-    public static Message createInviteRequest(SipProvider sip_provider,  //实时视频
-                                              NameAddress to, NameAddress from, String body) {
-        String via_addr = sip_provider.getViaAddress();
-        int host_port = sip_provider.getPort();
-        SipURL sipURL = new SipURL(via_addr, host_port);
-        NameAddress contact = new NameAddress(sipURL);
-        Message msg = createInviteRequest(sip_provider, sipURL, to, from, contact, null);
-        msg.setBody(CONTENT_TYPE, body);
-        return msg;
-    }
-
-    public static Message createByeRequest(SipProvider sip_provider,  //结束实时视频
-                                           NameAddress to, NameAddress from) {
-        String via_addr = sip_provider.getViaAddress();
-        int host_port = sip_provider.getPort();
-        SipURL sipURL = new SipURL(via_addr, host_port);
-        NameAddress contact = new NameAddress(sipURL);
-        return createRequest(sip_provider, SipMethods.BYE, sipURL, to, from, contact, null);
+    public static Message createByeRequest(NameAddress to, NameAddress from) {
+        return createRequest(SipUserManager.getInstance(), SipMethods.BYE, sipURL, to, from, contact, null);
     }
 
     public static Message createResponse(Message req, int code, String reason, String body) {
+        if (TextUtils.isEmpty(body)) {
+            body = "";
+        }
         return createResponse(req, code, reason, null, null, CONTENT_TYPE, body);
     }
 }
