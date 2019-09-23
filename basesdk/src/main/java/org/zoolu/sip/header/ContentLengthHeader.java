ace827d23af4d66dbb4ba06ac2514aef0c08bd27
han.chen <han.chen@beibei.com>
2019-09-23 22:05:07 +0800

    format

--- basesdk/src/main/java/org/zoolu/sip/header/ContentLengthHeader.java
+++ basesdk/src/main/java/org/zoolu/sip/header/ContentLengthHeader.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (C) 2005 Luca Veltri - University of Parma - Italy
- * 
+ *
  * This file is part of MjSip (http://www.mjsip.org)
- * 
+ *
  * MjSip is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * MjSip is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with MjSip; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  * Author(s):
  * Luca Veltri (luca.veltri@unipr.it)
  */
@@ -27,29 +27,34 @@ package org.zoolu.sip.header;
 import org.zoolu.sip.provider.SipParser;
 
 
-/** SIP Header Content-Length */
-public class ContentLengthHeader extends Header
-{
-   //public ContentLengthHeader()
-   //{  super(SipHeaders.Content_Length);
-   //}
+/**
+ * SIP Header Content-Length
+ */
+public class ContentLengthHeader extends Header {
+    //public ContentLengthHeader()
+    //{  super(SipHeaders.Content_Length);
+    //}
 
-   public ContentLengthHeader(int len)
-   {  super(SipHeaders.Content_Length,String.valueOf(len));
-   }
+    public ContentLengthHeader(int len) {
+        super(SipHeaders.Content_Length, String.valueOf(len));
+    }
 
-   public ContentLengthHeader(Header hd)
-   {  super(hd);
-   }
+    public ContentLengthHeader(Header hd) {
+        super(hd);
+    }
 
-   /** Gets content-length of ContentLengthHeader */
-   public int getContentLength()
-   {  return (new SipParser(value)).getInt();
-   }
+    /**
+     * Gets content-length of ContentLengthHeader
+     */
+    public int getContentLength() {
+        return (new SipParser(value)).getInt();
+    }
 
-   /** Set content-length of ContentLengthHeader */
-   public void setContentLength(int cLength)
-   {  value=String.valueOf(cLength);
-   }
+    /**
+     * Set content-length of ContentLengthHeader
+     */
+    public void setContentLength(int cLength) {
+        value = String.valueOf(cLength);
+    }
 
 }
--- basesdk/src/main/java/org/zoolu/sip/header/RequireHeader.java
+++ basesdk/src/main/java/org/zoolu/sip/header/RequireHeader.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (C) 2005 Luca Veltri - University of Parma - Italy
- * 
+ *
  * This file is part of MjSip (http://www.mjsip.org)
- * 
+ *
  * MjSip is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * MjSip is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with MjSip; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  * Author(s):
  * Luca Veltri (luca.veltri@unipr.it)
  */
@@ -24,16 +24,15 @@
 package org.zoolu.sip.header;
 
 
+/**
+ * SIP Header Require
+ */
+public class RequireHeader extends OptionHeader {
+    public RequireHeader(String option) {
+        super(SipHeaders.Require, option);
+    }
 
-
-/** SIP Header Require */
-public class RequireHeader extends OptionHeader
-{
-   public RequireHeader(String option)
-   {  super(SipHeaders.Require,option);
-   }
-
-   public RequireHeader(Header hd)
-   {  super(hd);
-   }
+    public RequireHeader(Header hd) {
+        super(hd);
+    }
 }
