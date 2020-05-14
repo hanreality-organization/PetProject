cc77a17ce378b869cef45d72c093e277a5e19e3a
han.chen <han.chen@beibei.com>
2020-05-14 13:16:24 +0800

    健康统计支持 日周月 的时间切换

--- module_home/src/main/java/com/punuo/pet/home/device/request/GetFoodFrequencyRequest.java
+++ module_home/src/main/java/com/punuo/pet/home/device/request/GetFoodFrequencyRequest.java
@@ -4,8 +4,15 @@ import com.punuo.pet.home.device.model.ChartData;
 import com.punuo.sys.sdk.httplib.BaseRequest;
 
 public class GetFoodFrequencyRequest extends BaseRequest<ChartData> {
-    public GetFoodFrequencyRequest(){
+
+    public GetFoodFrequencyRequest(ChartType chartType) {
         setRequestType(RequestType.GET);
-        setRequestPath("/weightshistogram/getEatFrequency");
+        if (chartType == ChartType.DAY) {
+            setRequestPath("/weightshistogram/getDayEatFrequency");
+        } else if (chartType == ChartType.WEEK) {
+            setRequestPath("/weightshistogram/getWeekEatFrequency");
+        } else if (chartType == ChartType.MONTH) {
+            setRequestPath("/weightshistogram/getMonthEatFrequency");
+        }
     }
 }
