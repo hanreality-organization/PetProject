b7ff8882bd293a54dcb3c6a889966f4e0192704b
han.chen <han.chen@beibei.com>
2021-05-27 15:20:38 +0800

    修改上传脚本

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
+
+# 此脚本依赖一个"jq"库，mac可以通过brew install jq安装
+
 while read -r line; do
   result=$(echo "$line" | grep "versionCode")
   if [ "$result" != "" ]; then
@@ -24,18 +27,35 @@ while read -r line; do
 done <base.gradle
 echo "准备开始打包"
 ./gradlew assembleRelease
-if [ $? -eq 0 ]; then
+
+updateVersion() {
+    response=$(curl -s "http://pet.qinqingonline.com/developers/appUpgrade?versionName=${versionName}&versionCode=${versionCode}")
+    result=$(echo "$response" | sed 's/.*success\":\([a-z]*\),.*/\1/g')
+    if [ "$result" = 'true' ]; then
+        echo "版本更新成功"
+    else
+        echo "版本更新失败"
+    fi
+}
+
+uploadFile() {
     echo "打包完成,开始上传apk"
     response=$(curl --location --request POST 'http://pet.qinqingonline.com/devupgrade/moveDevfile' --form 'updateFile=@"/Users/han.chen/punuo/PetProject/app/build/outputs/apk/release/feed_signed.apk"' --form 'fileName="feed_signed.apk"')
     result=$(echo "$response" | sed 's/.*success\":\([a-z]*\),.*/\1/g')
+    message=$(echo "$response" | sed 's/.*message\":\"\([^\x00-\xff]*\)\".*/\1/g')
     if [ "$result" = 'true' ]; then
         echo "上传完毕，更新版本信息"
-        response=$(curl -s "http://pet.qinqingonline.com/developers/appUpgrade?versionName=${versionName}&versionCode=${versionCode}")
-        result=$(echo "$response" | sed 's/.*success\":\([a-z]*\),.*/\1/g')
-        if [ "$result" = 'true' ]; then
-            echo "版本更新成功"
-            else
-              echo "版本更新失败"
-        fi
+        updateVersion
+    else
+        echo "$message"
     fi
+    return 1
+}
+
+if [ $? -eq 0 ]; then
+    uploadFile
 fi
+
+
+
+
